4d815966fe01ec79a391b3019654b2f1b2684cf6
Marsel <63933205+iammarsel@users.noreply.github.com>
2021-12-05 09:49:27 -0500

    Update trunk_speed4.py

--- trunk_speed4.py
+++ trunk_speed4.py
@@ -16,9 +16,9 @@ d1 = today_date.strftime("%m%d%y")
 d2 = today_date.strftime("%m/%d/%y")
 ios_l2 = {
 	'device_type': 'cisco_ios',
-	'username':'wilcom',
+	'username':'user1234',
 	'ip': '',
-	'password': '0p3nsky+',
+	'password': 'pAssW0rD',
 	'conn_timeout': 9999999
 }
 gap = 1
